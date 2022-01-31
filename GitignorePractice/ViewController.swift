bbcd470ef565442d59dd43dc79d3f0a4118a14bd
Minji Kim <minji_0801@naver.com>
2022-01-31 19:40:32 +0900

    Initial Commit

---
+++ GitignorePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitignorePractice
+//
+//  Created by 김민지 on 2022/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
